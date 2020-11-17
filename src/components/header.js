c9dcf85dc838e61c89539bae442b0d1a382ae6ee
Tommaso Mannori <aivirth.dev@gmail.com>
2020-11-17 02:03:59 +0100

    Added scss modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import headerStyles from "./header.module.scss";
+
 const Header = () => {
+  const { header, title, navList, activeNavItem } = headerStyles;
+
   const siteLinks = [
     {
       url: "/",
@@ -21,14 +25,24 @@ const Header = () => {
     },
   ];
   return (
-    <header>
-      <h1>Site title PH</h1>
+    <header className={header}>
+      <h1>
+        <Link to="/" className={title}>
+          Aivirth gatsby site
+        </Link>
+      </h1>
       <nav>
-        <ul>
-          {siteLinks.map(link => {
+        <ul className={navList}>
+          {siteLinks.map((link, index) => {
             return (
-              <li>
-                <Link to={link.url}>{link.name}</Link>
+              <li key={index}>
+                <Link
+                  className={headerStyles.navItem}
+                  to={link.url}
+                  activeClassName={activeNavItem}
+                >
+                  {link.name}
+                </Link>
               </li>
             );
           })}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
+import "../styles/reset.scss";
+import "../styles/index.scss";
+
+import layoutStyles from "./layout.module.scss";
 
 const Layout = props => {
   const { children } = props;
+  const { container, content } = layoutStyles;
   return (
-    <div>
-      <Header />
-      {children}
+    <div className={container}>
+      <div className={content}>
+        <Header />
+        {children}
+      </div>
+
       <Footer />
     </div>
   );
